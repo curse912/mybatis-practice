4f681429e5ffc90c977158fa16453b65875efa72
curse912 <obking0014@gmail.com>
2025-06-30 17:53:37 +0900

    2025-06-30 mybatis

--- src/main/java/com/kh/practice/common/SqlSessionTemplate.java
+++ src/main/java/com/kh/practice/common/SqlSessionTemplate.java
@@ -1,8 +1,29 @@
 package com.kh.practice.common;
 
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 public class SqlSessionTemplate {
-    // static 블럭을 사용하여 클래스 로딩시 sqlSessionFactory객체를 생성.
-	// 생성방법은 이전 교안 혹은 MyBatis메뉴얼 참고.
-    
+    // static 블럭을 사용하여 클래스 로딩시 sqlSessionFactory객체를 생성. 생성방법은 이전 교안 혹은 MyBatis메뉴얼 참고.
+	private static SqlSessionFactory sqlSessionFactory;
+	static {
+		String resource = "/mybatis-config.xml";
+		try {
+			InputStream inputStream = Resources.getResourceAsStream(resource);
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
     // SqlSession을 반환하는 메서드 작성
+	public static SqlSession getSession() {
+		return sqlSessionFactory.openSession(false);
+	}
 }
